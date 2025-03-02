92f7a706dc0e639f0341f5d479adf1b4befc6195
yagyagoel1 <yagyagoel87@gmail.com>
2025-03-02 20:39:41 +0530

    fixed the error dropping connection on end

--- cmd/quickdb/main.go
+++ cmd/quickdb/main.go
@@ -10,73 +10,79 @@ import (
 	"github.com/yagyagoel1/quickdb/utils"
 )
 
-func main() {
-	fmt.Println("listening on port 6379")
-	l, err := net.Listen("tcp", ":6379")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	aof, err := aof.NewAof("database.aof")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer aof.Close()
-
-	aof.Read(func(value utils.Value) {
-		command := strings.ToUpper(value.Array[0].Bulk)
-		args := value.Array[1:]
-		handler, ok := handler.Handlers[command]
-		if !ok {
-			fmt.Println("Invalid command: ", command)
-			return
-		}
-
-		handler(args)
-	})
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+func handleConnection(conn net.Conn, aofInstance *aof.Aof) {
 	defer conn.Close()
 	for {
 		resp := utils.NewResp(conn)
 		value, err := resp.Read()
 		if err != nil {
-			fmt.Println(err)
-			return
+			fmt.Println("Connection error:", err)
+			break
 		}
 
-		fmt.Println(value)
+		fmt.Println("Received:", value)
 		if value.Typ != "array" {
-			fmt.Println("invalid request , expected array")
+			fmt.Println("invalid request, expected array")
+			continue
 		}
 		if len(value.Array) == 0 {
-			fmt.Println("invalid requrest expected array length more than 0")
+			fmt.Println("invalid request, expected array length more than 0")
+			continue
 		}
+
 		command := strings.ToUpper(value.Array[0].Bulk)
 		args := value.Array[1:]
-
 		writer := utils.NewWriter(conn)
-		handler, ok := handler.Handlers[command]
+		cmdHandler, ok := handler.Handlers[command]
 		if !ok {
 			fmt.Println("invalid command", command)
-			writer.Write(utils.Value{
-				Typ: "string", Str: "",
-			})
+			writer.Write(utils.Value{Typ: "string", Str: ""})
 			continue
 		}
 		if command == "SET" || command == "HSET" {
-			aof.Write(value)
+			aofInstance.Write(value)
 		}
 
-		result := handler(args)
+		result := cmdHandler(args)
+		fmt.Println("result", result)
 		writer.Write(result)
-		// ignore request and send back a PONG
-		// conn.Write([]byte("+OK\r\n"))
 	}
+}
 
+func main() {
+	fmt.Println("Listening on port 6379")
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Listener error:", err)
+		return
+	}
+	defer l.Close()
+
+	aofInstance, err := aof.NewAof("database.aof")
+	if err != nil {
+		fmt.Println("AOF error:", err)
+		return
+	}
+	defer aofInstance.Close()
+
+	aofInstance.Read(func(value utils.Value) {
+		command := strings.ToUpper(value.Array[0].Bulk)
+		args := value.Array[1:]
+		cmdHandler, ok := handler.Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command:", command)
+			return
+		}
+		cmdHandler(args)
+	})
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Accept error:", err)
+			continue
+		}
+
+		go handleConnection(conn, aofInstance)
+	}
 }
